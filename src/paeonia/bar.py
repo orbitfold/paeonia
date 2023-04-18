4096aa4e4c52e47b9649369bd8e94f4d9016929c
Vytautas Jancauskas <unaudio@gmail.com>
2023-04-18 14:53:07 +0200

    Update parse in bar.py

--- src/paeonia/bar.py
+++ src/paeonia/bar.py
@@ -1,10 +1,20 @@
+import re
+
 class Bar:
     def __init__(self, string, time_sig=(4, 4), key_sig=[]):
         pass
     
     def parse(self, string):
-        dur_regexp = r'[whqestxuy]'
-        note_regexp = r'[cdefgab][0-9]'
+        dur_regexp = re.compile(r'-?[whqestxuy].?.?.?')
+        note_regexp = re.compile(r'[CDEFGAB](s?|b?)[0-9]')
+        tokens = string.split(" ")
+        for token in tokens:
+            if dur_regexp.match(token):
+                pass
+            elif note_regexp.match(token):
+                pass
+            else:
+                raise RuntimeError(f"Invalid token! ({token})")
         
     def to_midi(self, channel):
         pass
