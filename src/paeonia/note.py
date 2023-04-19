f52245cdac2686d4b2246198862a80e8f0b10769
Vytautas Jancauskas <unaudio@gmail.com>
2023-04-19 17:44:13 +0200

    Update note.py

--- src/paeonia/note.py
+++ src/paeonia/note.py
@@ -1,3 +1,12 @@
+import numpy as np
+
 class Note:
     def __init__(self):
-        pass
+        self.distribution = np.ones(127)
+        self.normalize()
+    
+    def normalize(self):
+        self.distribution = self.distribution / self.distribution.sum()
+
+    def instantiate(self):
+        return np.random.choice(list(range(127)), p=self.distribution)
