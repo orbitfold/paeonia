2d4eaae33d25e2bf7ec18a9c5f5acc7e20571936
Vytautas Jancauskas <unaudio@gmail.com>
2023-04-25 16:04:00 +0200

    Update note.py

--- src/paeonia/note.py
+++ src/paeonia/note.py
@@ -1,16 +1,22 @@
 import numpy as np
 
 class Note:
-    def __init__(self, note=None, duration=1):
+    def __init__(self, note=None, duration=1.0):
         if note is None:
             self.distribution = np.ones(127)
+            self.normalize()
         else:
-            self.distribution = np.zeros(127)
-            self.distribution[note] = 1.0
-        self.normalize()
+            self.note = note
+        self.duration = duration
     
     def normalize(self):
         self.distribution = self.distribution / self.distribution.sum()
 
-    def instantiate(self):
+    @property
+    def note(self):
         return np.random.choice(list(range(127)), p=self.distribution)
+    
+    @note.setter
+    def note(self, value):
+        self.distribution = np.zeros(127)
+        self.distribution[value] = 1.0
