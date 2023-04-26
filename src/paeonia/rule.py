2b71e8c44197c1c90dd1352e2af11ee87027d477
Vytautas Jancauskas <unaudio@gmail.com>
2023-04-26 14:08:41 +0200

    Create rule.py

---
+++ src/paeonia/rule.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class Scale:
+  def __init__(self, root=0, mode=0, strength=1.0):
+    scale = np.array([1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1])
+  
+  @property
+  def distribution(self):
+    pass
