edf6a7d1388bfec14e3a7d19806c9c162e19c9fd
Vytautas Jancauskas <unaudio@gmail.com>
2023-04-27 14:19:08 +0200

    Update bar.py

--- src/paeonia/bar.py
+++ src/paeonia/bar.py
@@ -1,12 +1,12 @@
+import mido
+
+# [1/4, n1, n2, [n1, n2], -1/4]
+
 class Bar:
-    def __init__(self):
-        self.notes = {}
-    
-    def add_note(self, note, offset):
-        try:
-            self.notes[offset].append(note)
-        except KeyError:
-            self.notes[offset] = [note]
+    def __init__(self, bar):
+        self.bar = bar
         
-    def to_midi(self, channel):
-        pass
+    def to_midi(self):
+        duration = 1.0
+        for event in self.bar:
+            pass
