ceb4f7de81f096242980f653ffa63dca5aad1c47
Vytautas Jancauskas <unaudio@gmail.com>
2023-05-05 17:14:30 +0200

    Update rule.py

--- src/paeonia/rule.py
+++ src/paeonia/rule.py
@@ -26,3 +26,19 @@ class Range:
     distribution[self.a:self.b] = np.ones(self.b - self.a)
     distribution = distribution / distribution.sum()
     return distribution
+
+  
+class Walk:
+  def __init__(self, note, weights=[1, 1, 2, 2, 3, 3, 1, 3, 3, 2, 2, 1, 1]):
+    self.note = note
+    self.weights = weights
+    
+  @property
+  def distribution(self):
+    distribution = np.zeros(127)
+    n_weights = len(self.weights)
+    n_half = math.floor(n_weights / 2.)
+    last_note = self.note.history[-1]
+    distribution[last_note - n_half:last_note + n_half + 1] = self.weights
+    return distribution
+    
