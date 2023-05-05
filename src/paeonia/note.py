216cec651b7f8160af5d8d02ee84137b6e8c2849
Vytautas Jancauskas <unaudio@gmail.com>
2023-05-05 23:32:47 +0200

    Update note.py

--- src/paeonia/note.py
+++ src/paeonia/note.py
@@ -3,7 +3,7 @@ import numpy as np
 class Note:
     def __init__(self, note=None):
         self.chain = []
-        self.history = []
+        self._history = []
         
     @property
     def note(self):
@@ -11,8 +11,8 @@ class Note:
         for rule in self.chain[1:]:
             distribution = rule.distribution * distribution
             distribution = distribution / distribution.sum()
-        self.history.append(np.random.choice(list(range(127)), p=distribution))
-        return self.history[-1]
-
+        self._history.append(np.random.choice(list(range(127)), p=distribution))
+        return self._history[-1]
+        
     def append(self, rule):
         self.chain.append(rule)
