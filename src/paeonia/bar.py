334e9bcd401cebbb1fc4607dd8f0afdff056c642
Vytautas Jancauskas <unaudio@gmail.com>
2025-02-20 16:25:02 +0100

    Update bar.py

--- src/paeonia/bar.py
+++ src/paeonia/bar.py
@@ -3,4 +3,11 @@ class Bar:
         self.notes = notes
 
     def add_note(self, note):
+        """Append a new note to the bar.
+
+        Parameters
+        ----------
+        note: Note
+           A note to add
+        """
         self.notes.append(note)
