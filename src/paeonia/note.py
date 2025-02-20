54eb318d7630ac50019d7ff4b13cee3441862b46
Vytautas Jancauskas <unaudio@gmail.com>
2025-02-20 10:23:40 +0100

    Update note.py

--- src/paeonia/note.py
+++ src/paeonia/note.py
@@ -1,5 +1,5 @@
 class Note:
-  def __init__(self, pitches, duration, velocity):
-    self.pitches = pitches
-    self.duration = duration
-    self.velocity = velocity
+    def __init__(self, pitches, duration, velocity):
+        self.pitches = pitches
+        self.duration = duration
+        self.velocity = velocity
