c262e021d12e0c84fca075607deb9248bc46e4bc
Vytautas Jancauskas <unaudio@gmail.com>
2025-02-20 16:30:12 +0100

    Update score.py

--- src/paeonia/score.py
+++ src/paeonia/score.py
@@ -1,8 +1,11 @@
 class Score:
     def __init__(self):
-        pass
+        self.voices = []
 
     def to_midi(self, path):
         for voice in self.voices:
             for bar in voice.bars:
                 pass
+                
+    def add_voice(self, voice):
+        self.voices.append(voice)
