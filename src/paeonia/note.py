fbc54105445c9ccc1736b60ac647773de006a51d
Vytautas Jancauskas <unaudio@gmail.com>
2025-02-21 11:23:59 +0100

    Update note.py

--- src/paeonia/note.py
+++ src/paeonia/note.py
@@ -1,5 +1,16 @@
+from mido import Message
+
 class Note:
     def __init__(self, pitches, duration, velocity):
         self.pitches = pitches
         self.duration = duration
         self.velocity = velocity
+        
+    def to_midi(self, tpb=480):
+        """Return MIDI messages corresponding to this node.
+        """
+        for index, pitch in enumerate(self.pitches):
+            Message('note_on', note=int(pitch), velocity=int(127 * self.velocity), time=(0 if index < len(self.pitches) - 1 else int(tpb * 4 * self.duration)))
+        for index, pitch in enumerate(self.pitches):
+            Message('note_off', note=int(pitch), velocity=127, time=0)
+
