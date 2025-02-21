c0e62d777c29b907976211d9e5f2d07796f438b5
Vytautas Jancauskas <unaudio@gmail.com>
2025-02-21 13:07:06 +0100

    Update note.py

--- src/paeonia/note.py
+++ src/paeonia/note.py
@@ -1,4 +1,7 @@
 from mido import Message
+from paeonia.utils import download_sf2
+import fluidsynth
+import time
 
 class Note:
     def __init__(self, pitches, duration, velocity):
@@ -16,3 +19,16 @@ class Note:
             messages.append(Message('note_off', note=int(pitch), velocity=127, time=0))
         return messages
 
+    def preview(self, fs=None):
+        """Preview a note using fluidsynth.
+        """
+        if fs is None:
+            fs = fluidsynth.Synth()
+            fs.start()
+            sfid = fs.sfload(download_sf2())
+            fs.program_select(0, sfid, 0, 0)
+        for index, pitch in enumerate(self.pitches):
+            fs.noteon(0, pitch, self.velocity)
+        time.sleep(self.duration)
+        for index, pitch in enumerate(self.pitches):
+            fs.noteoff(0, pitch)
