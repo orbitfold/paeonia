a60938b2cf08a63ca69dd7a25d5f8ace3cd79698
Vytautas Jancauskas <unaudio@gmail.com>
2025-02-21 11:45:15 +0100

    Update note.py

--- src/paeonia/note.py
+++ src/paeonia/note.py
@@ -14,4 +14,5 @@ class Note:
             messages.append(Message('note_on', note=int(pitch), velocity=int(127 * self.velocity), time=(0 if index < len(self.pitches) - 1 else int(tpb * 4 * self.duration))))
         for index, pitch in enumerate(self.pitches):
             messages.append(Message('note_off', note=int(pitch), velocity=127, time=0))
+        return messages
 
