b10dcf9e1593ea0e622ab326a5bf303a0e6aee9f
Vytautas Jancauskas <unaudio@gmail.com>
2025-03-03 23:13:08 +0100

    test relative mode

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,5 +1,6 @@
 from paeonia.parser import np
 import paeonia
+from fractions import Fraction
 
 def test_np():
     test_input = "C#4. D''8.. R2. Bb,16 <F# A# C#'>2. G, R E4.. <A C' E>1"
@@ -7,5 +8,17 @@ def test_np():
     pitches = [[49], [74], None, [46], [54, 58, 61], [43], None, [52], [57, 60, 52]]
     for note, pitch_list in zip(parsed_bar.notes, pitches):
         assert(note.pitches == pitch_list)
+    durations = [Fraction('3/8'), Fraction('1/4'), Fraction('3/4'), Fraction('1/16'),
+                 Fraction('3/4'), Fraction('1/4'), Fraction('1/4'), Fraction('1/2'),
+                 Fraction('1/1')]
+    for note, duration in zip(parsed_bar.notes, durations):
+        assert(note.duration == duration)
+    parsed_bar = np(test_input, relative=True)
+    pitches = [[49], [74], None, [70], [66, 70, 73], [67], None, [64], [69, 72, 76]]
+    for note, pitch_list in zip(parsed_bar.notes, pitches):
+        assert(note.pitches == pitch_list)
+    for note, duration in zip(parsed_bar.notes, durations):
+        assert(note.duration == duration)
+        
 
 
