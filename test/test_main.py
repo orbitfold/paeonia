47924ea746c4576e77fadb1c36ed058a5a79e813
Vytautas Jancauskas <unaudio@gmail.com>
2025-03-04 19:49:02 +0100

    get rid of the old parser

--- test/test_main.py
+++ test/test_main.py
@@ -28,17 +28,4 @@ def test_download_sf2():
     file = download_sf2()
     assert(os.path.isfile(file))
 
-def test_parsing_notes():
-    note1 = Note("c'")
-    assert(note1.pitches == [60])
-    assert(note1.duration == 0.25)
-    note2 = Note("a1")
-    assert(note2.pitches == [57])
-    assert(note2.duration == 1.0)
-    note3 = Note("gs'''8.")
-    assert(note3.pitches == [92])
-    assert(note3.duration == 0.1875)
-    note4 = Note("ff,,4")
-    assert(note4.pitches == [28])
-    assert(note4.duration == 0.25)
     
