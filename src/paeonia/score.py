7277ee34c92c87920b577e0e59429a5df8fbc53e
Vytautas Jancauskas <unaudio@gmail.com>
2025-03-07 16:57:25 +0100

    implemented show in score

--- src/paeonia/score.py
+++ src/paeonia/score.py
@@ -1,8 +1,37 @@
-from mido import Message, MidiFile, MidiTrack
+from mido import Message, MidiFile, MidiTrack, MetaMessage
+import tempfile
+from paeonia.utils import download_sf2
+import os
+import subprocess
+import importlib
+from string import Template
+import tempfile
+from IPython.display import display, Image
 
 class Score:
     def __init__(self):
-        self.voices = []
+        self.voices = {}
+        self.clefs = {}
+
+    def __getitem__(self, idx):
+        return self.voices[idx]
+
+    def __setitem__(self, idx, voice):
+        self.voices[idx] = voice
+        self.clefs[idx] = "treble"
+
+    def set_clef(self, voice, clef):
+        """Set a type of clef used for a voice.
+
+        Parameters
+        ----------
+        voice: str
+            Voice name
+        clef: str
+            Clef name (treble, alto, tenor, bass)
+        """
+        assert(clef in ["treble", "alto", "tenor", "bass"])
+        self.clefs[voice] = clef
 
     def to_midi(self, path, tpb=480):
         """Write the score to MIDI file.
@@ -13,22 +42,38 @@ class Score:
             A filename to write to.
         """
         mid = MidiFile(ticks_per_beat=tpb)
-        for voice in self.voices:
+        for key in self.voices:
             track = MidiTrack() 
+            voice = self[key]
+            track += voice.to_midi(tpb)
+            track.append(MetaMessage('end_of_track', time=0))
             mid.tracks.append(track)
-            for bar in voice.bars:
-                for note in bar.notes:
-                    for pitch in note.pitches:
-                        track.append(Message('note_on', note=int(pitch), velocity=int(127 * note.velocity), time=int(4 * tpb * note.duration)))
-                        track.append(Message('note_off', note=int(pitch), velocity=127, time=0))
         mid.save(path)
-                
-    def add_voice(self, voice):
-        """Add a voice to this score.
 
-        Parameters
-        ----------
-        voice: Voice
-            A Voice to add to the Score.
+    def show(self):
+        """Attempts to render a lilypond file and display it on a Jupyter notebook.
         """
-        self.voices.append(voice)
+        template = Template(importlib.resources.open_text('paeonia.data', 'score_template.ly').read())
+        with tempfile.TemporaryDirectory() as tmpdir:
+            score_lilypond = []
+            for voice_name in self.voices:
+                score_lilypond.append("\\new Staff")
+                score_lilypond.append(f"{{ \clef {self.clefs[voice_name]} {self.voices[voice_name].to_lilypond()} \\break}}")
+            score_notation = "\n".join(score_lilypond)
+            notation = template.substitute(notation=score_notation)
+            with open(os.path.join(tmpdir, 'notation.ly'), 'w') as fd:
+                fd.write(notation)
+            subprocess.run(['lilypond', '-dpreview', '--loglevel=ERROR',
+                            '-fpng', os.path.join(tmpdir, 'notation.ly')], cwd=tmpdir)
+            display(Image(filename=os.path.join(tmpdir, 'notation.png')))
+        return self
+
+    def play(self, tpb=480):
+        """Preview the score using fluidsynth
+        """
+        with tempfile.TemporaryDirectory() as tmpdir:
+            self.to_midi(os.path.join(tmpdir, 'score.mid'), tpb)
+            sf_file = download_sf2()
+            subprocess.run(['fluidsynth', '-i', sf_file, os.path.join(tmpdir, 'score.mid')],
+                           stdout=subprocess.DEVNULL)
+        return self
