cbb2350a1dbec5a3302452733fbf3ef23acc47b7
Vytautas Jancauskas <unaudio@gmail.com>
2025-03-15 13:18:10 +0100

    updates to tonality

--- src/paeonia/tonality.py
+++ src/paeonia/tonality.py
@@ -1,12 +1,17 @@
 from itertools import cycle
+from paeonia.utils import note_name_to_pitch_class, mode_name_to_index
 
 class Tonality:
-    def __init__(self, root='C', mode='major'):
+    def __init__(self, root='C', mode='ionian'):
         intervals = [2, 2, 1, 2, 2, 2, 1]
+        mode_index = mode_name_to_index(mode)
+        intervals = intervals[mode_index:] + intevals[:-mode_index]
         interval_cycle = cycle(intervals)
-        self.pitches = [0]
+        self.pitches = [-12 + note_name_to_pitch_class(root)]
         while True:
             new_pitch = self.pitches[-1] + next(interval_cycle)
+            if new_pitch < 0:
+                continue
             if new_pitch > 127:
                 break
             self.pitches.append(new_pitch)
--- src/paeonia/utils.py
+++ src/paeonia/utils.py
@@ -5,6 +5,78 @@ import pathlib
 import tempfile
 from mido import MidiFile, MidiTrack
 
+_TRANSLATE_NOTE_NAME = {
+    "C": 0,
+    "C#": 1,
+    "Cb": 11
+    "D": 2,
+    "D#": 3,
+    "Db": 1
+    "E": 4,
+    "E#": 5,
+    "Eb": 3,
+    "F": 5,
+    "F#": 6,
+    "Fb": 4,
+    "G": 7,
+    "G#": 8,
+    "Gb": 6,
+    "A": 9,
+    "A#": 10,
+    "Ab": 8,
+    "B": 11,
+    "B#": 0,
+    "Bb": 10
+}
+
+_TRANSLATE_MODE_NAME = {
+    "major": 0,
+    "ionian": 0,
+    "dorian": 1,
+    "phrygian": 2,
+    "lydian": 3,
+    "mixolydian": 4,
+    "minor": 5,
+    "aeolian": 5,
+    "locrian": 6
+}
+
+def note_name_to_pitch_class(note_name):
+    """Translate note name to pich class.
+
+    Parameters
+    ----------
+    note_name: str
+        A note name in paeonia format but without octave.
+
+    Returns
+    -------
+    int
+        Pitch class (0 to 11)
+    """
+    try:
+        return _TRANSLATE_NOTE_NAME[note_name]
+    except KeyError:
+        raise RuntimeError(f"Note name must be in {list(_TRANSLATE_NOTE_NAME)}!")
+
+def mode_name_to_index(mode_name):
+    """Translate mode name to tranposition index.
+
+    Parameters
+    ----------
+    mode_name: str
+        Mode name.
+
+    Returns
+    -------
+    int:
+        Index
+    """
+    try:
+        return _TRANSLATE_MODE_NAME[mode_name]
+    except KeyError:
+        raise RuntimeError(f"Mode name must be in {list(_TRANSLATE_MODE_NAME)}!")
+
 def download_sf2():
     """Download the GM soundfont to be used with fluidsynth.
     """
