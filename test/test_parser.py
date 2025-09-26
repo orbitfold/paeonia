8a4fdfc4ee0f8803b571662a5486b6cd5df3f460
Vytautas Jancauskas <unaudio@gmail.com>
2025-09-26 15:00:50 +0200

    Update test_parser.py

--- test/test_parser.py
+++ test/test_parser.py
@@ -5,7 +5,7 @@ from fractions import Fraction
 def test_parse():
     test_input = "C#4. D''8.. R2. Bb,16 <F# A# C#'>2. G, R E4.. <A C' E>1"
     parsed_bar = parse(test_input, relative=False)
-    pitches = [[61], [86], [], [58], [66, 70, 73], [55], [], [65], [69, 72, 64]]
+    pitches = [[61], [86], [], [58], [66, 70, 73], [55], [], [64], [69, 72, 64]]
     for note, pitch_list in zip(parsed_bar, pitches):
         assert(note.pitches == pitch_list)
     durations = [Fraction('3/8'), Fraction('1/4'), Fraction('3/4'), Fraction('1/16'),
