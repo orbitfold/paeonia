72532b8ab6955a77fdaaafa803e59c64eb9e3f99
Vytautas Jancauskas <unaudio@gmail.com>
2026-07-04 17:57:56 +0200

    added initial triadex muse implementation

--- src/paeonia/__init__.py
+++ src/paeonia/__init__.py
@@ -3,3 +3,15 @@ from .bar import Bar
 from .voice import Voice
 from .score import Score
 from .tonality import Tonality
+from .triadex import MuseFrame, TriadexMuse, muse_widget
+
+__all__ = [
+    "Note",
+    "Bar",
+    "Voice",
+    "Score",
+    "Tonality",
+    "MuseFrame",
+    "TriadexMuse",
+    "muse_widget",
+]
